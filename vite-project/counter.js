39d963c6e5f1bcf4ffff4246148b77b1ec02e4dc
BSelyomin <bogdan.selyomin@gmail.com>
2022-11-16 13:34:28 -0500

    11/16

--- vite-project/counter.js
+++ vite-project/counter.js
@@ -1,9 +1,17 @@
 export function setupCounter(element) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+}
+
+let array = ["touch", "touch", "touch", "touch"];
+
+let touch = "touch";
+
+for (let hi of array) {
+  console.log(`${entry.events.length} events.`);
 }
